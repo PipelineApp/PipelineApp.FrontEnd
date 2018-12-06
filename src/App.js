cd2d8554b3a9eac79fdc3245e9e6de33c0e81993
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-12-06 15:17:17 -0600

    Updates for analytics and layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import React from 'react';
-// import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import 'react-redux-toastr/src/styles/index.scss';
 import 'rc-tooltip/assets/bootstrap.css';
 
 import '../scss/style.scss';
-// import history from './utility/history';
+import history from './utility/history';
 // import * as actions from './infrastructure/actions';
+import LandingContainer from './display/containers/LandingContainer';
+import LayoutContainer from './display/containers/LayoutContainer';
+import MaintenanceContainer from './display/containers/MaintenanceContainer'
 
 const propTypes = {
+	isMaintenanceMode: PropTypes.bool.isRequired
 };
 
-function mapStateToProps() {
-	return {};
+function mapStateToProps(state) {
+	const {
+		ui
+	} = state;
+	return {
+		isMaintenanceMode: ui.isMaintenanceMode
+	};
 }
 
-const App = () => (<span>Hello World</span>);
-
+const App = (props) => {
+	const { isMaintenanceMode } = props;
+	if (isMaintenanceMode) {
+		return (
+			<Router history={history}>
+				<Route path="*" name="Maintenance" component={MaintenanceContainer} />
+			</Router>
+		);
+	}
+	return (
+		<Router history={history}>
+			<Switch>
+				<Route
+					path="/maintenance"
+					name="Maintenance"
+					component={MaintenanceContainer}
+				/>
+				{
+					['/login', '/forgotpassword', '/resetpassword', '/register'].map(path => <Route key={path} path={path} component={LandingContainer} />)
+				}
+				<Route component={LayoutContainer} />
+			</Switch>
+		</Router>
+	);
+};
 App.propTypes = propTypes;
 export default connect(mapStateToProps)(App);
--- src/infrastructure/actions/index.js
+++ src/infrastructure/actions/index.js
@@ -1,1 +1,11 @@
-// barrel up actions here
+// Account
+export {
+	SUBMIT_USER_LOGOUT,
+	submitUserLogout
+} from './user/submitUserLogout';
+
+// UI
+export {
+	setMaintenanceModeOn,
+	SET_MAINTENANCE_MODE_ON
+} from './ui/setMaintenanceModeOn';
